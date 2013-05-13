ca28bf846f382366e1f0b388e89038b017f8e642
Akiva Leffert <akiva@etsy.com>
2013-05-13 11:27:39 -0400

    Create initial project

---
+++ Knobs/Knobs/EKNAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  EKNAppDelegate.m
+//  Knobs
+//
+//  Created by Akiva Leffert on 5/13/13.
+//  Copyright (c) 2013 Akiva Leffert. All rights reserved.
+//
+
+#import "EKNAppDelegate.h"
+
+@implementation EKNAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Knobs/Knobs/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Knobs
+//
+//  Created by Akiva Leffert on 5/13/13.
+//  Copyright (c) 2013 Akiva Leffert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
